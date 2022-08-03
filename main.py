e052c4cc5009af2cb494eeb6cab6c71bd68f63f2
Mauricio Obando Gomez <mauricio.obando.-nd@disney.com>
2022-08-02 20:22:57 -0500

    adding ufo api modifying first structure

--- main.py
+++ main.py
@@ -37,4 +37,24 @@ def get_ufos(ufos_id:str):
     for ufo in ufos_array:
         if ufo['id']==ufos_id:
             return ufo
-    raise HTTPException(status_code=404.,detail="Post Not Found")
+    raise HTTPException(status_code=404.,detail="Ufo Not Found")
+
+
+@app.delete("/ufos/{ufos_id}")
+def delete_ufo(ufos_id:str):
+    for index,ufo in enumerate(ufos_array):
+        if ufo["id"]==ufos_id:
+            ufos_array.pop(index)
+            return {"message": "Ufo has been deleted successfully"}
+
+    raise HTTPException(status_code=404.,detail="Ufo Not Found")
+
+@app.put('/ufos/{ufo_id}')
+def update_ufo(ufo_id:str,updateUfo:Ufo):
+    for index,ufo in enumerate(ufos_array):
+        if ufo['id']==ufo_id:
+            ufos_array[index]["location_ufo"]=updateUfo.location_ufo
+            ufos_array[index]["description"]=updateUfo.description
+            ufos_array[index]["published"]=updateUfo.published
+            return {"message": "Ufo has been Updated successfully"}
+    raise HTTPException(status_code=404.,detail="Ufo Not Found")
