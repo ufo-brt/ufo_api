d9225c720a6fb974a50261b61622e9cbab071ca3
mauricioobgo <leidybelro23@gmail.com>
2023-10-10 17:10:16 -0500

    Adding change on enviroment execution

--- config/db.py
+++ config/db.py
@@ -2,3 +2,4 @@ from pymongo import MongoClient
 import os
 
 conn=MongoClient(f"mongodb+srv://{os.environ.get('USER_MONGO')}:{os.environ.get('MONGO_KEY')}@ufobrt.wyofdo3.mongodb.net/?retryWrites=true&w=majority")
+
